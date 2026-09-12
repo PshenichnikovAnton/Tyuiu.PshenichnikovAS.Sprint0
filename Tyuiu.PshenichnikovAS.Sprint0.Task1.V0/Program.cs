13da2f20ac26b25ec95990076d2f54be73c04aed
Антон Пшеничников <PshenichnikovAS@outlook.com>
2026-09-12 17:30:06 +0500

    Task1

---
+++ Tyuiu.PshenichnikovAS.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,8 @@
+﻿// See  for more information
+Console.WriteLine("Hello, World!");
+string name = "Антон";
+int age = 17;
+Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+
+
+
