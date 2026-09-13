8aa7fcb2d76b15e5ea55ab4aaf42b9e9a179b74f
Антон Пшеничников <PshenichnikovAS@outlook.com>
2026-09-13 20:05:07 +0500

    Task6

---
+++ Tyuiu.PshenichnikovAS.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,8 @@
+﻿using Tyuiu.PshenichnikovAS.Sprint0.Task6.V0.Lib;
+
+int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(numsArray));
+Console.WriteLine("Разность элементов массива = " + DataService.SubstractionArray(numsArray));
+Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(numsArray));
+Console.ReadKey();
+
